e4560356ce2e624018b30ec9e61d10ea8c9a6201
toan.pn <toanphamhsgs@gmail.com>
2025-06-26 04:11:27 +0700

    revert config local

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
 	production: true,
 	hashbrown: {
 		config: {
-			baseUrl: 'http://localhost:5001/api/hashbrown',
+			baseUrl: '/api/hashbrown',
 		},
 	},
-	apiUrl: 'http://localhost:5001/api',
+	apiUrl: '/api',
 };
